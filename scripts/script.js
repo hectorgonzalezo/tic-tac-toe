1f5bd39035a9acc5b5bd5b9a5f5831032ccd159c
Hector Gonzalez <gorozco.hector@gmail.com>
2022-08-24 10:09:52 -0400

    Make popup correctly extract players' names

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ const gameBoard = (
         let _board = new Array(9).fill('');
 
         const restart = function () {
-            _board = new Array(9).fill(''); 
+            _board = new Array(9).fill('');
             displayController.render(_board)
         }
 
@@ -21,41 +21,41 @@ const gameBoard = (
             return _board.every((cell) => cell != '')
         }
 
-        const _checkWin = function(){
+        const _checkWin = function () {
             return _checkWinHorizontal() || _checkWinVertical() || _checkWinDiagonal();
-            
+
         }
 
-        const _checkWinHorizontal = function(){
+        const _checkWinHorizontal = function () {
             //check if any line has three consecutive marks of any kind
             const win = _board.slice(0, 7).some((cell, i) => {
                 //check only every three marks if the next two are the same
-                return cell != '' && i%3 == 0 && cell == _board[i+1] && cell == _board[i+2]
+                return cell != '' && i % 3 == 0 && cell == _board[i + 1] && cell == _board[i + 2]
             })
             return win
         }
 
-        const _checkWinVertical = function() {
+        const _checkWinVertical = function () {
             //check vertical lines
             const win = _board.slice(0, 3).some((cell, i) => {
                 //check in the first row if the two below are the same
-                return cell != '' && cell == _board[i+3] && cell == _board[i+6]
+                return cell != '' && cell == _board[i + 3] && cell == _board[i + 6]
             })
-            return win 
+            return win
         }
 
-        const _checkWinDiagonal = function() {
+        const _checkWinDiagonal = function () {
             const win = _board.slice(0, 3).some((cell, i) => {
                 //check every index 0 and 2
-                if (i == 0 && cell != '' && cell == _board[4]){
-                    return cell ==_board[8]
-                } else if (i == 2 && cell != '' && cell == _board[4]){
+                if (i == 0 && cell != '' && cell == _board[4]) {
+                    return cell == _board[8]
+                } else if (i == 2 && cell != '' && cell == _board[4]) {
                     return cell == _board[6]
-                } else{
+                } else {
                     return false
                 }
             })
-            return win  
+            return win
         }
 
         return { update, restart }
@@ -70,27 +70,30 @@ const displayController = (
         const _stateDisplay = document.querySelector('#state-display')
         const _restartButton = document.querySelector('#restart-button')
         const _popup = document.querySelector('#pop-up')
+        const _popupForm = document.querySelector('#form-player-names')
         const _popupButton = document.querySelector('#pop-up-button')
-        const _visibleArea = document.querySelectorAll('.visible-area')
-        
+        const _visibleArea = document.querySelectorAll('#visible-area')
+
         //add event listeners to squares to update when pressed by player
         const activateCells = () => {
-        _gameSquares.forEach(
-            (square) => square.addEventListener('click', _cellListenerFunc))
+            _gameSquares.forEach(
+                (square) => square.addEventListener('click', _cellListenerFunc))
         };
 
         const _deactivateCells = () => {
+            console.log('deact')
             _gameSquares.forEach(
                 (square) => square.removeEventListener('click', _cellListenerFunc))
         }
 
-        const _cellListenerFunc = function(){
-            game.turn(this.getAttribute('data'))
+        const _cellListenerFunc = function () {
+            game.turn(this.getAttribute('data'));
+            console.log(this)
         }
 
         //restart with button
         _restartButton.addEventListener('click', () => game.restart());
-        
+
         //updates DOM
         const render = function (board) {
             _gameSquares.forEach((square, i) => {
@@ -100,28 +103,40 @@ const displayController = (
 
         //show game after pressing start button in pop up
         _popupButton.addEventListener('click', (e) => {
-            _visibleArea.forEach((area) => area.classList.toggle('inactive'))
-            _popup.classList.add('inactive')
+            e.preventDefault();
+            if (_popupForm.checkValidity()) {
+                _visibleArea.forEach((area) => area.classList.toggle('invisible'));
+                _popup.classList.toggle('invisible');
+
+                const formData = new FormData(_popupForm)
+                const newPlayerNames = Object.fromEntries(formData.entries())
+
+                //create Players
+                player1 = Player(newPlayerNames['player1'], '0');
+                player2 = Player(newPlayerNames['player2'], 'x')
+
+                game.start();
+            }
         })
 
-        const changeStateDisplay = function (player, win=false, tie=false) {
+        const changeStateDisplay = function (player, win = false, tie = false) {
             let text
             //get the name of the other player to post whose turn is next
-            const nextPlayer = player1.getName() == player ? 
-            player2.getName() : 
-            player1.getName();
+            const nextPlayer = player1.getName() == player ?
+                player2.getName() :
+                player1.getName();
 
             if (win) {
-                text = `${player} won.` 
+                text = `${player} won.`
                 _deactivateCells();
-            } else if(tie) {
+            } else if (tie) {
                 text = `It's a tie!`
             } else {
-            text = `${nextPlayer}'s turn.`
+                text = `${nextPlayer}'s turn.`
             }
             _stateDisplay.innerText = text;
         }
-        return {render, changeStateDisplay, activateCells}
+        return { render, changeStateDisplay, activateCells }
     })();
 
 //factory function to create a player
@@ -132,25 +147,25 @@ const Player = function (name, mark) {
         gameBoard.update(cellNum, _mark, name);
     }
 
-    const getName = function(){
+    const getName = function () {
         return name
     }
 
-    return {addMark, getName}
+    return { addMark, getName }
 }
 
 //manages the flow of the game.
 const game = (function () {
     let counter = 0;
 
-    const start = function(){
+    const start = function () {
         displayController.changeStateDisplay(player2.getName())
         displayController.activateCells();
     };
 
-    const turn = function(cellNum) {
+    const turn = function (cellNum) {
         //alternate turns between players
-        if(counter%2 == 0){
+        if (counter % 2 == 0) {
             player1.addMark(cellNum);
         } else {
             player2.addMark(cellNum);
@@ -158,18 +173,16 @@ const game = (function () {
         counter++
     }
 
-    const restart = function() {
+    const restart = function () {
         counter = 0;
         gameBoard.restart();
         this.start();
     }
-    return{start, turn, restart}
+    return { start, turn, restart }
 }
 )()
 
 
 
-let player1 = Player('Hector', '0');
-let player2 = Player('Juan', 'x')
-
-game.start()
+let player1
+let player2
